f09245c24161d0e10b39f29adc811ce7a0f29c47
Manvith Polisetty <manvith.polisetty@gmail.com>
2025-08-11 15:50:34 +0800

    birthday

---
+++ birthday.py
@@ -0,0 +1,11 @@
+Birthday1="1990/01/01"
+Birthday2="2000/09/09"
+Birthday3="2005/10/10"
+Birthday4="2013/05/05"
+Birthday5="2018/12/12"
+
+print(Birthday1)
+print(Birthday2)
+print(Birthday3)
+print(Birthday4)
+print(Birthday5)
---
+++ birthday1.py
@@ -0,0 +1,7 @@
+Birthday1="1990/01/01"
+Birthday2="2000/09/09"
+Birthday3="2005/10/10"
+Birthday4="2013/05/05"
+Birthday5="2018/12/12"
+
+print(f'Birthdates are {Birthday1}, {Birthday2}, {Birthday3}, {Birthday4}, {Birthday5}')
